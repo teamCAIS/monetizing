ea0dea831814c55ec939c7e5feee33f6e75c878a
Caio Nunes <caionunes3000@gmail.com>
2019-12-04 02:09:06 -0300

    Updates

---
+++ precache-manifest.a903a208b1bc4715506ef06d4888d94e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a32865d8f16e23b0eacedbbe16c96b4",
+    "url": "/monetizing/index.html"
+  },
+  {
+    "revision": "5f4e20d27bd9e4bc2b05",
+    "url": "/monetizing/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d3b69f1c93229382000f",
+    "url": "/monetizing/static/css/main.297736af.chunk.css"
+  },
+  {
+    "revision": "5f4e20d27bd9e4bc2b05",
+    "url": "/monetizing/static/js/2.e0d5371a.chunk.js"
+  },
+  {
+    "revision": "d3b69f1c93229382000f",
+    "url": "/monetizing/static/js/main.6377a672.chunk.js"
+  },
+  {
+    "revision": "b3fe32893a74a7bcfb7b",
+    "url": "/monetizing/static/js/runtime-main.67a344a9.js"
+  }
+]);
